309a76182908d78a755cc8777f3ba5dd091d0027
ADITYA SINHA <aditya.sinha2026@vitstudent.ac.in>
2026-09-20 23:22:44 +0530

    Add files via upload
    add user login panel with case-sensitive validation

---
+++ User_Login_Pannel.py
@@ -0,0 +1,28 @@
+print("---USER LOGIN PANNEL---")
+while True:
+    username=input("Enter your username:")
+    password=input("Enter your password(atleast 6 characters):")
+    if len(password)<6:
+        print("Error!! Password must be atleast 6 characters long")
+    else:
+        print("Login Successful")
+        print("Welcome to the system....",username)
+        break
+print("===User Login Pannel===")
+attempts_left=3
+while attempts_left>0:
+    Login_Username=input("Enter your username:")
+    Login_Password=input("Enter your password:")
+    if Login_Username==username and Login_Password==password:
+        print("Login Successful")
+        break
+    else:
+        print("Error!! Invalid Username or Password")
+        attempts_left-=1
+        print("Attempts Left:",attempts_left)
+if attempts_left==0:
+    print("Error!! You have exceeded the maximum number of attempts. Please try again later.")
+
+
+
+ 
